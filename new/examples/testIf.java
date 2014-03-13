637f3e620ead689db46ee3ba0f00a3c87fe45c17
Clyde Byrd III <byrdc@purdue.edu>
2014-03-13 02:23:06 -0400

    test files

---
+++ new/examples/testIf.java
@@ -0,0 +1,22 @@
+class Simple {
+	public static void main(String[] a) {
+		System.out.println(new Sim().sum(20));
+	}
+}
+
+class Sim {
+		public int sum(int i) {
+			int[] a;
+
+			a = new int[i];
+
+			if(1 > 0) {
+				a[0] = 1;
+			}
+
+			a[0] = 2;
+			
+			System.out.println(a[0]);
+			return 1;
+		}
+}
---
+++ new/examples/testMember.java
@@ -0,0 +1,14 @@
+class Factorial{
+    public static void main(String[] a){
+	System.out.println(new Fac().ComputeFac(10));
+    }
+}
+
+class Fac {
+	int n;
+    public int ComputeFac(int num){
+	n = 2;
+	return n;
+    }
+
+}
