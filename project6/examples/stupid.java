7b11dbe228dc496cbd6af9d02f422a133433af92
Clyde Byrd III <byrdc@purdue.edu>
2014-04-24 14:23:09 -0400

    new files

---
+++ project6/examples/stupid.java
@@ -0,0 +1,66 @@
+class stupid{
+    public static void main(String[] a){
+	System.out.println((new Fac()).ComputeFac(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18));	
+	
+    
+    }
+}
+
+class Fac{
+	
+	public int ComputeFac(int f1, int f2, int f3, int f4, int f5, int f6, int f7, int f8, int f9, int f10, int f11, int f12, int f13, int f14, int f15,int f16, int f17, int f18){
+		
+		
+
+		return f1+f2+f3+f4+f5+f6+f7+f8+f9+f10+f11+f12+f13+f14+f15+f16+f17+f18;
+	}
+}
+class A{
+	int f1;
+	int c;
+
+	public int cool11(){
+		this.c=1;
+		return 1;
+	}
+	/*
+	public int ComputeFac(int[] a, int b){
+		//a[b];
+		//a[b]=b;
+		if (3==3){
+			a[3]=3;
+		}
+		System.out.println(b);
+		return 1;
+	}
+	*/
+}
+/*
+class B extends A{
+	int f1;
+
+	public int cool(int a){
+		return a;
+	}
+}
+*/
+/*
+class Fac {
+    
+    public int ComputeFac(int a, int b, boolean c, boolean d){
+	a<b;
+	a<=b;
+	a==b;
+	a!=b;
+	a>=b;
+	a>b;
+	
+	!c;
+	c&&d;
+	c||d;
+
+	return 1;
+    }
+
+}
+*/
--- project6/examples/test.java
+++ project6/examples/test.java
@@ -1,23 +1,5 @@
-class Factorial{
-    public static void main(String[] a){
-		System.out.println(new Fac().ComputeFac(10));
-    }
-}
-
-class Fac {
-
-    public int ComputeFac(int num){
-	int x;
-	int y;
-
-	x = 10 % num;
-	y = 5  % num;
-	System.out.println(x);
-	System.out.println(y);
-	System.out.println(13 % num);
-	System.out.println(100 % num);
-	System.out.println(500213 % 57);
-	return 0;
-    }
-
+class Test{
+	public static void main(String[] a){
+		true==false;
+	}
 }
